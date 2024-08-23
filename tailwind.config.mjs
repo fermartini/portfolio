3dd7dedc5f9517be290bd7252f4abc759123a31c
fermartini1 <hola.soymartini1992@gmail.com>
2024-08-23 17:33:30 -0300

    portfolio hasta proyectos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,21 +3,37 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			animation: {
+				squeeze: "squeeze 0.6s ease-in-out",
+				slide1: "slide 0.2s ease-out",
+				slide2: "slide 0.4s ease-out",
+				slide3: "slide 0.6s ease-out",
+				slide4: "slide 0.8s ease-out"
+
+			},
+			keyframes: {
+				squeeze: {
+					"0%, 100%": {
+						transform: "scale(1, 1)"
+					},
+					"50%": {
+						transform: "scale(1.1, 0.9)"
+					}
+				},
+				slide: {
+					"0%": {
+						transform: "translateY(-20px)"
+					},
+					"100%": {
+						transform: "translateY(0)"
+					}
+				}
+
+			},
 			
-		},
+		}
 	},
 	plugins: [{
-		"animation": {
-		  "squeeze": "squeeze 0.6s ease-in-out"
-		},
-		"squeeze": {
-		  "0%, 100%": {
-			"transform": "scale(1, 1)"
-		  },
-		  "50%": {
-			"transform": "scale(1.1, 0.9)"
-		  }
-		}
-	  }],
+	}]
 	
 }
